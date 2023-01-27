16ab2fc30296fe04267dd397e8dae6bfa0c4c491
Jesstern Rays <jsstrn@users.noreply.github.com>
2023-01-28 00:29:30 +0800

    Replace item names with constants

--- src/Shop.js
+++ src/Shop.js
@@ -1,3 +1,5 @@
+const {AGED_BRIE, BACKSTAGE_PASS, SULFURAS} = require("./constants")
+
 class Shop {
     constructor(items = []) {
         this.items = items;
@@ -5,11 +7,11 @@ class Shop {
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
             if (
-                this.items[i].name != "Aged Brie" &&
-                this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+                this.items[i].name != AGED_BRIE &&
+                this.items[i].name != BACKSTAGE_PASS
             ) {
                 if (this.items[i].quality > 0) {
-                    if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+                    if (this.items[i].name != SULFURAS) {
                         this.items[i].quality = this.items[i].quality - 1;
                     }
                 }
@@ -17,7 +19,7 @@ class Shop {
                 if (this.items[i].quality < 50) {
                     this.items[i].quality = this.items[i].quality + 1;
                     if (
-                        this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+                        this.items[i].name == BACKSTAGE_PASS
                     ) {
                         if (this.items[i].sellIn < 11) {
                             if (this.items[i].quality < 50) {
@@ -32,16 +34,16 @@ class Shop {
                     }
                 }
             }
-            if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (this.items[i].name != SULFURAS) {
                 this.items[i].sellIn = this.items[i].sellIn - 1;
             }
             if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != "Aged Brie") {
+                if (this.items[i].name != AGED_BRIE) {
                     if (
-                        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+                        this.items[i].name != BACKSTAGE_PASS
                     ) {
                         if (this.items[i].quality > 0) {
-                            if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+                            if (this.items[i].name != SULFURAS) {
                                 this.items[i].quality = this.items[i].quality - 1;
                             }
                         }
